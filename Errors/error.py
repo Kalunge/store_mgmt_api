73186e1a462de94ab17231a5a4f2d43ed00e206d
Kalungez <muthomititus+github@gmail.com>
2020-04-03 22:14:18 +0300

    move all error handlers to new file

---
+++ Errors/error.py
@@ -0,0 +1,13 @@
+from main import app, Unauthorized, NotFound, BadRequest, jsonify
+
+@app.errorhandler(400)
+def Unauthorized(error):
+    return jsonify({'message' :'You are not authorized'})
+
+@app.errorhandler(404)
+def NotFound(error):
+    return jsonify({'message':'the resource you requested is not availabe'})
+
+@app.errorhandler(400)
+def BadRequest(error):
+    return jsonify({'message':'that was a bad request'})
